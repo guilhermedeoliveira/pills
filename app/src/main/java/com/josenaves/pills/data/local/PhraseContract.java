c91277d6d2093f250c1d62f40d2eac5750659382
José Naves Moura Neto <josenaves@gmail.com>
2016-09-16 18:03:10 -0300

    Criação do banco de dados + tabela phrase

---
+++ app/src/main/java/com/josenaves/pills/data/local/PhraseContract.java
@@ -0,0 +1,17 @@
+package com.josenaves.pills.data.local;
+
+import android.provider.BaseColumns;
+
+/**
+ * The contract used by database to save phrases in Sqlite
+ */
+public interface PhraseContract {
+    interface PhraseEntry extends BaseColumns {
+        String TABLE_NAME = "phrase";
+
+        String COLUMN_NAME_PHRASE = "phrase";
+        String COLUMN_NAME_AUTHOR = "author";
+        String COLUMN_NAME_VIEWS = "views";
+        String COLUMN_NAME_SHARED = "shared";
+    }
+}
